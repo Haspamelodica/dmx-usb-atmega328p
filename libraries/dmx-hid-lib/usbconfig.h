b9f836a877aa2585b4e182e65bbbad16e5c9b955
Haspamelodica <melodicahaspa@gmail.com>
2021-12-29 23:11:59 +0100

    Fixed HID usbconfig.h

--- libraries/dmx-hid-lib/usbconfig.h
+++ libraries/dmx-hid-lib/usbconfig.h
@@ -249,7 +249,6 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
- */
 #define USB_CFG_DEVICE_NAME     'D', 'M', 'X', '-', 'H', 'I', 'D'
 #define USB_CFG_DEVICE_NAME_LEN 7
 /* Same as above for the device name. If you don't want a device name, undefine
